0d9c83b20d61efadcb89c4bb3aef35ce4149b6d7
ainugiri <40005294+ainugiri@users.noreply.github.com>
2024-02-09 16:07:03 +0530

    Update Sample01.java

--- Sample01.java
+++ Sample01.java
@@ -6,7 +6,7 @@ class Sample01{
 		System.out.println("Jenkins Triggered from GitHub Repository");
 		System.out.println("Jenkins Triggered from GitHub Repository");
 		System.out.println("Jenkins Triggered from GitHub Repository");
-		System.out.println("Jenkins Triggered from GitHub Repository");
+		System.out.println("Jenkins https://9c58cc08f091ffb078427d937dce91dd.loophole.site from GitHub Repository");
 		System.out.println("Jenkins Triggered from GitHub Repository");
 		System.out.println("Jenkins Triggered from GitHub Repository");
 		System.out.println("Jenkins Triggered from GitHub Repository");
