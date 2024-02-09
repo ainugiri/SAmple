020fe85d9a9987ff964e51d2e1d1a81a48c1f4d4
ainugiri <40005294+ainugiri@users.noreply.github.com>
2024-02-09 15:47:50 +0530

    Update Sample01.java

--- Sample01.java
+++ Sample01.java
@@ -3,5 +3,8 @@ class Sample01{
 	public static void main(String args[])
 	{
 		System.out.println("Jenkins Triggered from GitHub Repository");
+		System.out.println("Jenkins Triggered from GitHub Repository");
+		System.out.println("Jenkins Triggered from GitHub Repository");
+		System.out.println("Jenkins Triggered from GitHub Repository");
 	}
 }
