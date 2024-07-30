a914cfac9ec0f3cd5fcff310811aeac4c743202d
ainugiri <40005294+ainugiri@users.noreply.github.com>
2024-07-30 11:42:17 +0530

    Update Sample01.java

--- Sample01.java
+++ Sample01.java
@@ -2,15 +2,6 @@ class Sample01{
 
 	public static void main(String args[])
 	{
-		System.out.println("Load from GitHub Repo and replace the code in workspace");
-		System.out.println("Jenkins https://.loophole.site from GitHub Repository");
-		System.out.println("Load from GitHub Repo and replace the code in workspace");
-		System.out.println("Jenkins https://.loophole.site from GitHub Repository");
-		System.out.println("Load from GitHub Repo and replace the code in workspace");
-		System.out.println("Jenkins https://.loophole.site from GitHub Repository");
-		System.out.println("Load from GitHub Repo and replace the code in workspace");
-		System.out.println("Jenkins https://.loophole.site from GitHub Repository");
-		System.out.println("Load from GitHub Repo and replace the code in workspace");
-		System.out.println("Jenkins https://.loophole.site from GitHub Repository");
+		System.out.println("Sample01.java");
 	}
 }
