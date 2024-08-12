7daf4d686054384f68f4bd587fe6df0945362ca5
ainugiri <40005294+ainugiri@users.noreply.github.com>
2024-08-12 11:08:27 +0530

    Update Sample01.java

--- Sample01.java
+++ Sample01.java
@@ -2,6 +2,15 @@ class Sample01{
 
 	public static void main(String args[])
 	{
-		System.out.println("11:23 09-08-2024");
+		System.out.println("11:23 09-08-2024")
+			
+		System.out.println("11:23 09-08-2024")
+			
+		System.out.println("11:23 09-08-2024")
+			
+		System.out.println("11:23 09-08-2024")
+			
+		System.out.println("11:23 09-08-2024")
+			
 	}
 }
