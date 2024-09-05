8308563d23b6d0956a0898164d599279a4372534
ainugiri <40005294+ainugiri@users.noreply.github.com>
2024-09-05 16:11:44 +0530

    Update Sample01.java by GIri

--- Sample01.java
+++ Sample01.java
@@ -2,13 +2,6 @@ class Sample01{
 
 	public static void main(String args[])
 	{
-		System.out.println("11:23 09-08-2024");
-		System.out.println("11:23 09-08-2024");
-		System.out.println("11:23 09-08-2024");
-		System.out.println("11:23 09-08-2024");
-		System.out.println("11:23 09-08-2024");
-		System.out.println("11:23 09-08-2024");
-		System.out.println("11:23 09-08-2024");
-		System.out.println("11:23 09-08-2024");			
+		System.out.println("GITHUB Updated the execution");		
 	}
 }
